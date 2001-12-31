6aff1ab4175cb42e333ad47183339db6a821a92b
Stefan Petersen <spe@stacken.kth.se>
2001-12-31 00:45:35 +0000

    Drawing aperture macros with some elementary macro primitives

--- src/draw.c
+++ src/draw.c
@@ -120,6 +120,255 @@ gerbv_draw_arc(GdkPixmap *pixmap, GdkGC *gc,
 
 
 /*
+ * Stack declarations and operations
+ */
+typedef struct {
+    double *stack;
+    int sp;
+} stack_t;
+
+stack_t *
+new_stack(void)
+{
+    const int stack_size = 20;
+    stack_t *s;
+
+    s = (stack_t *)malloc(sizeof(stack_t));
+    if (!s) {
+	free(s);
+	return NULL;
+    }
+    memset(s, 0, sizeof(stack_t));
+
+    s->stack = (double *)malloc(sizeof(double) * stack_size);
+    if (!s->stack) {
+	free(s->stack);
+	return NULL;
+    }
+
+    memset(s->stack, 0, sizeof(double) * stack_size);
+    s->sp = 0;
+
+    return s;
+} /* new_stack */
+
+
+static void
+free_stack(stack_t *s)
+{
+    if (s && s->stack)
+	free(s->stack);
+
+    if (s)
+	free(s);
+
+    return;
+} /* free_stack */
+
+static void
+print_stack(stack_t *s)
+{
+    int i;
+
+    printf("%d:",s->sp);
+    for (i = 0; i < s->sp; i++)
+	printf("%f  ", s->stack[i]);
+    printf("\n");
+    
+    return;
+} /* print_stack */
+
+static void
+push(stack_t *s, double val)
+{
+    s->stack[s->sp++] = val;
+    return;
+} /* push */
+
+
+static double
+pop(stack_t *s)
+{
+    return s->stack[--s->sp];
+} /* pop */
+
+
+/*
+ * Doesn't handle exposure yet and explicit x,y
+ */
+static void
+gerbv_draw_prim5(GdkPixmap *pixmap, GdkGC *gc, stack_t *s, int scale,
+		 gint x, gint y)
+{
+    const int nuf_vertices_pos = 1;
+    const int diameter_pos = 4;
+    const int rotation_pos = 5;
+    int nuf_vertices, i;
+    double vertex, tick, rotation, radius;
+    GdkPoint *points;
+
+    if (s->sp != 6)
+	return;
+
+    nuf_vertices = (int)s->stack[nuf_vertices_pos];
+    points = (GdkPoint *)malloc(sizeof(GdkPoint) * nuf_vertices);
+    if (!points) {
+	free(points);
+	return;
+    }
+
+    /*
+     * We calculate everything in degrees first, since rotation
+     * position is in degrees.
+     */
+    tick = 2 * M_PI / (double)nuf_vertices;
+    rotation = s->stack[rotation_pos] * M_PI / 180.0;
+    radius = s->stack[diameter_pos] / 2.0;
+    for (i = 0; i < nuf_vertices; i++) {
+	vertex =  tick * (double)i + rotation;
+	points[i].x = (int)round(scale * radius * cos(vertex)) + x;
+	points[i].y = (int)round(scale * radius * sin(vertex)) + y;
+    }
+
+    gdk_draw_polygon(pixmap, gc, 1, points, nuf_vertices);
+
+    free(points);
+    return;
+} /* gerbv_draw_prim5 */
+
+
+/*
+ * Doesn't handle exposure yet, explicit x,y and rotation
+ */
+void
+gerbv_draw_prim20(GdkPixmap *pixmap, GdkGC *gc, stack_t *s, int scale,
+		  gint x, gint y)
+{
+    const int linewidth_pos = 1;
+    const int start_x_pos = 2;
+    const int start_y_pos = 3;
+    const int end_x_pos = 4;
+    const int end_y_pos = 5;
+    /* const int rotation_pos = 6; */
+    GdkGC *local_gc = gdk_gc_new(pixmap);
+    int x1, y1, x2, y2;
+
+    gdk_gc_copy(local_gc, gc);
+
+    gdk_gc_set_line_attributes(local_gc, 
+			       (int)round(scale * s->stack[linewidth_pos]),
+			       GDK_LINE_SOLID, 
+			       GDK_CAP_BUTT, 
+			       GDK_JOIN_MITER);
+
+    x1 = (s->stack[start_x_pos] * scale) + x;
+    y1 = (s->stack[start_y_pos] * scale) + y;
+    x2 = (s->stack[end_x_pos] * scale) + x;
+    y2 = (s->stack[end_y_pos] * scale) + y;
+
+    gdk_draw_line(pixmap, local_gc, x1, y1, x2, y2);
+    
+    gdk_gc_unref(local_gc);
+
+    return;
+} /* gerbv_draw_prim20 */
+
+
+/*
+ * Doesn't handle exposure yet, explicit x,y and rotation
+ */
+void
+gerbv_draw_prim21(GdkPixmap *pixmap, GdkGC *gc, stack_t *s, int scale,
+		  gint x, gint y)
+{
+    const int width_pos = 1;
+    const int height_pos = 2;
+    /*const int rotation_pos = 5;*/
+    const int nuf_points = 4;
+    GdkPoint points[nuf_points];
+    double half_width, half_height;
+
+    half_width = (int)round(s->stack[width_pos] * scale / 2.0);
+    half_height =(int)round(s->stack[height_pos] * scale / 2.0);
+
+    points[0].x = half_width + x;
+    points[0].y = half_height + y;
+
+    points[1].x = half_width + x;
+    points[1].y = -half_height + y;
+
+    points[2].x = -half_width + x;
+    points[2].y = -half_height + y;
+
+    points[3].x = -half_width + x;
+    points[3].y = half_height + y;
+
+    gdk_draw_polygon(pixmap, gc, 1, points, nuf_points);
+
+    return;
+} /* gerbv_draw_prim21 */
+
+
+static int
+gerbv_draw_amacro(GdkPixmap *pixmap, GdkGC *gc,
+		  instruction_t *program, double *parameters, int scale,
+		  gint x, gint y)
+{
+    stack_t *s = new_stack();
+    instruction_t *ip;
+    int handled = 1;
+
+    for(ip = program; ip != NULL; ip = ip->next) {
+	switch(ip->opcode) {
+	case NOP:
+	    break;
+	case PUSH :
+	    push(s, ip->data.fval);
+	    break;
+        case PPUSH :
+	    push(s, parameters[ip->data.ival - 1]);
+	    break;
+	case ADD :
+	    push(s, pop(s) + pop(s));
+	    break;
+	case SUB :
+	    push(s, -pop(s) + pop(s));
+	    break;
+	case MUL :
+	    push(s, pop(s) * pop(s));
+	    break;
+	case DIV :
+	    push(s, 1 / ((pop(s) / pop(s))));
+	    break;
+	case PRIM :
+	    switch(ip->data.ival) {
+	    case 5 :
+		gerbv_draw_prim5(pixmap, gc, s, scale, x, y);
+		break;
+	    case 2  :
+	    case 20 :
+		gerbv_draw_prim20(pixmap, gc, s, scale, x, y);
+		break;
+	    case 21 :
+		gerbv_draw_prim21(pixmap, gc, s, scale, x, y);
+		break;
+	    default :
+#if 0
+		printf("Unknown/unhandled primitive %d\n", ip->data.ival);
+		print_stack(s);
+#endif
+		handled = 0;
+	    }
+	    break;
+	default :
+	}
+    }
+    free_stack(s);
+    return handled;
+} /* gerbv_draw_amacro */
+
+
+/*
  * Convert a gerber image to a GTK pixmap to be displayed
  */
 int
@@ -260,8 +509,17 @@ image2pixmap(GdkPixmap **pixmap, struct gerb_image *image,
 		gerbv_draw_circle(*pixmap, line_gc, TRUE, x2, y2, p1);
 		break;
 	    case MACRO :
-		gdk_gc_set_foreground(err_gc, err_color);
-		gerbv_draw_circle(*pixmap, err_gc, TRUE, x2, y2, scale/20);
+		/*
+		 * While aperture macros are not properly implemented
+		 * we do like this in the mean time.
+		 */
+		if (!gerbv_draw_amacro(*pixmap, line_gc, 
+				       image->aperture[net->aperture]->amacro->program,
+				       image->aperture[net->aperture]->parameter,
+				       scale, x2, y2)) {
+		    gdk_gc_set_foreground(err_gc, err_color);
+		    gerbv_draw_circle(*pixmap, err_gc, TRUE, x2, y2, scale/20);
+		}
 		break;
 	    default :
 		err(1, "Unknown aperture type\n");
