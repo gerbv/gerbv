92608af6eed8b81c1e3a1d9c558c56c2ab801b61
Holger Waechtler <holger@convergence.de>
2003-03-22 22:05:51 +0000

    oops, forgot this one: Date: 2003/03/18 22:18:48 Author: spetm Log: Improved with check for tag name and recursive tagging

--- cvs-tag.sh
+++ cvs-tag.sh
@@ -3,15 +3,21 @@
 # Tags files according to FILES2TAG. Entries with # in the beginning will
 # not be tagged.
 
-# (C) 2002, Stefan Petersen (spe@stacken.kth.se)
+# (C) 2002-2003, Stefan Petersen (spe@stacken.kth.se)
 # $Id$
 
 FILES2TAG=files2tag.txt
 CVS=cvs
 TAG=$1
 
+if  test -z ${TAG} ; then
+    echo Give tag name
+    exit 1;
+fi
+
 for file2tag in `cat ${FILES2TAG}` ; do
     if  test -z "`echo ${file2tag} | mawk '$0 ~ /^#/'`"  ; then
-    ${CVS} tag -l ${TAG} ${file2tag}
+    echo TAGGING ${file2tag}
+    ${CVS} tag ${TAG} ${file2tag}
     fi
 done
