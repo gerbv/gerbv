5873c5b1a38cb978cf0328cc7f95c728b3b2ffaf
Julian Lamb <thepurlieu@gmail.com>
2008-04-17 02:50:22 +0000

    Hook up basic move object function (available through right-click menu).

--- src/callbacks.c
+++ src/callbacks.c
@@ -1834,7 +1834,7 @@ void
 callbacks_move_objects_clicked (GtkButton *button, gpointer   user_data){
 #ifndef RENDER_USING_GDK
 	/* for testing, just hard code in some translations here */
-	gerb_image_move_selected_objects (screen.selectionInfo.selectedNodeArray, -0.010, 0.010);
+	gerb_image_move_selected_objects (screen.selectionInfo.selectedNodeArray, -0.050, 0.050);
 	render_clear_selection_buffer ();
 	render_refresh_rendered_image_on_screen ();
 #endif
