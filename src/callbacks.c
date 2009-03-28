5565f83dcad4d1738ee6af22ba0a0ba1aa48d03b
Julian Lamb <thepurlieu@gmail.com>
2009-03-28 04:33:50 +0000

    * cleanup a spurious comment accidentally left in from previous commit

--- src/callbacks.c
+++ src/callbacks.c
@@ -1563,14 +1563,11 @@ void callbacks_update_scrollbar_positions (void){
 		positionX = ((GtkAdjustment *)screen.win.hAdjustment)->lower;
 	if (positionX > (((GtkAdjustment *)screen.win.hAdjustment)->upper - ((GtkAdjustment *)screen.win.hAdjustment)->page_size))
 		positionX = (((GtkAdjustment *)screen.win.hAdjustment)->upper - ((GtkAdjustment *)screen.win.hAdjustment)->page_size);
-	
 	gtk_adjustment_set_value ((GtkAdjustment *)screen.win.hAdjustment, positionX);
-	/* figure out mid-screen position */
 	
 	positionY = ((GtkAdjustment *)screen.win.vAdjustment)->upper - screenRenderInfo.lowerLeftY -
 		((GtkAdjustment *)screen.win.vAdjustment)->page_size +
 		((GtkAdjustment *)screen.win.vAdjustment)->lower;
-
 	if (positionY < ((GtkAdjustment *)screen.win.vAdjustment)->lower)
 		positionY = ((GtkAdjustment *)screen.win.vAdjustment)->lower;
 	if (positionY > (((GtkAdjustment *)screen.win.vAdjustment)->upper - ((GtkAdjustment *)screen.win.vAdjustment)->page_size))
