97bbf4067aa076177d5f2703ab06e50d7c4fa6a9
Sergey Alyoshin <alyoshin.s@gmail.com>
2013-06-29 22:12:56 +0400

    Add autopoint
    Add autopoint for internationalization support.

--- autogen.sh
+++ autogen.sh
@@ -6,6 +6,29 @@ rm -fr autom4te.cache
 
 ############################################################################
 #
+# autopoint (gettext)
+#
+
+echo "Checking autopoint version..."
+ver=`autopoint --version | awk '{print $NF; exit}'`
+ap_maj=`echo $ver | sed 's;\..*;;g'`
+ap_min=`echo $ver | sed -e 's;^[0-9]*\.;;g'  -e 's;\..*$;;g'`
+ap_teeny=`echo $ver | sed -e 's;^[0-9]*\.[0-9]*\.;;g'`
+echo "    $ver"
+
+case $ap_maj in
+	0)
+		if test $ap_min -lt 14 ; then
+			echo "You must have gettext >= 0.14.0 but you seem to have $ver"
+			exit 1
+		fi
+		;;
+esac
+echo "Running autopoint..."
+autopoint --force || exit 1
+
+############################################################################
+#
 # libtoolize (libtool)
 #
 
