57d59c8f2147f1e3d5bb31d7db8cb604a6c8175b
Sergey Alyoshin <alyoshin.s@gmail.com>
2014-08-10 22:06:20 +0400

    Add va_end() after va_start()

--- src/callbacks.c
+++ src/callbacks.c
@@ -131,6 +131,7 @@ void utf8_snprintf(gchar *dst, gsize byte_len, const gchar *fmt, ...)
 
 	va_start(ap, fmt);
 	gchar *str = g_strdup_vprintf(fmt, ap); 
+	va_end(ap);
 	utf8_strncpy(dst, str, byte_len);
 	g_free(str);
 }
