a9c65d81c2a4f0f9171e1f13338b697f0459546a
Sergey Alyoshin <alyoshin.s@gmail.com>
2018-09-26 00:29:35 +0300

    Clarity G85 as cut slot with series of holes

--- src/callbacks.c
+++ src/callbacks.c
@@ -1347,7 +1347,7 @@ callbacks_analyze_active_drill_activate(GtkMenuItem *menuitem,
 	table_add_row(G_table, "G05", stats_report->G05,
 			_("Drill mode"));
 	table_add_row(G_table, "G85", stats_report->G85,
-			_("Cut slot"));
+			_("Cut slot (series of holes)"));
 	table_add_row(G_table, "G90", stats_report->G90,
 			_("Absolute units"));
 	table_add_row(G_table, "G91", stats_report->G91,
