fc30735872dadeafeffbb0e73dead83b821efe5b
ooxi <violetland@mail.ru>
2023-05-26 20:26:10 +0200

    Update to Fedora 38 in CI
    We are only keeping the last release in CI in order to not slow down build times too much.

--- .mc/rebuild.sh
+++ .mc/rebuild.sh
@@ -3,7 +3,7 @@
 # Helper script to unify build logic between different operating systems. Can be
 # executed like this from the main directory:
 #
-#     mc fedora:36 .mc/rebuild.sh
+#     mc fedora:38 .mc/rebuild.sh
 #     mc ubuntu:22.04 .mc/rebuild.sh
 set -e
 
