146ce65f2fdd94cad408b3dca4fa8262b79b1715
Henry Gabryjelski <henrygab@users.noreply.github.com>
2023-10-17 21:53:00 -0700

    Code cleanup: variable scope changes

--- src/drill.c
+++ src/drill.c
@@ -1207,27 +1207,20 @@ drill_parse_T_code(gerb_file_t* fd, drill_state_t* state, gerbv_image_t* image, 
     if (drill_parse_T_needs_Orcad_Hack_or_EOF(fd, state, image, file_line)) {
         return -1;
     }
-
-    int                  tool_num;
-    gboolean             done = FALSE;
-    int                  temp;
-    double               size;
-    gerbv_drill_stats_t* stats = image->drill_stats;
-    gerbv_aperture_t*    apert;
-    gchar*               string;
+    gerbv_error_list_t* error_list = image->drill_stats->error_list;
 
     dprintf("---> entering %s()...\n", __FUNCTION__);
 
-    tool_num = (int)gerb_fgetint(fd, NULL);
+    int tool_num = (int)gerb_fgetint(fd, NULL);
     dprintf("  Handling tool T%d at line %ld\n", tool_num, file_line);
 
     if (tool_num == 0) {
         return tool_num; /* T00 is a command to unload the drill */
     }
-
+    /* Limit valid tools to be [1..9999] */
     if (tool_num < TOOL_MIN || tool_num >= TOOL_MAX) {
         gerbv_stats_printf(
-            stats->error_list, GERBV_MESSAGE_ERROR, -1,
+            error_list, GERBV_MESSAGE_ERROR, -1,
             _("Out of bounds drill number %d "
               "at line %ld in file \"%s\""),
             tool_num, file_line, fd->filename
@@ -1236,24 +1229,27 @@ drill_parse_T_code(gerb_file_t* fd, drill_state_t* state, gerbv_image_t* image, 
     }
 
     /* Set the current tool to the correct one */
-    state->current_tool = tool_num;
-    apert               = image->aperture[tool_num];
+    state->current_tool     = tool_num;
+    gerbv_aperture_t* apert = image->aperture[tool_num];
 
     /* Check for a size definition */
-    temp = gerb_fgetc(fd);
 
-    /* This bit of code looks for a tool definition by scanning for strings
-     * of form TxxC, TxxF, TxxS.  */
+    /* Repeatedly scan for tool definitions of form T#(F#|S#|C#)*
+     * At this point, have read 'T#' ...
+     */
+    int      next_char = gerb_fgetc(fd);
+    gboolean done      = FALSE;
     while (!done) {
-        switch ((char)temp) {
+        switch ((char)next_char) {
             case 'F':
             case 'S':
-                /* Silently ignored. They're not important. */
+                /* Silently ignored F/S and the integer that follows. They're not important. */
                 gerb_fgetint(fd, NULL);
                 break;
 
-            case 'C':
-                size = read_double(fd, state->header_number_format, GERBV_OMIT_ZEROS_TRAILING, state->decimals);
+            case 'C':  // C# provides a drill size
+                ;      // label must be part of a statement, so use empty statement.
+                double size = read_double(fd, state->header_number_format, GERBV_OMIT_ZEROS_TRAILING, state->decimals);
                 dprintf("  Read a size of %g\n", size);
 
                 if (state->unit == GERBV_UNIT_MM) {
@@ -1266,18 +1262,18 @@ drill_parse_T_code(gerb_file_t* fd, drill_state_t* state, gerbv_image_t* image, 
                        seemed a bit too small a margin, so a third it is */
 
                     gerbv_stats_printf(
-                        stats->error_list, GERBV_MESSAGE_ERROR, -1,
+                        error_list, GERBV_MESSAGE_ERROR, -1,
                         _("Read a drill of diameter %g inches "
                           "at line %ld in file \"%s\""),
                         size, file_line, fd->filename
                     );
-                    gerbv_stats_printf(stats->error_list, GERBV_MESSAGE_WARNING, -1, _("Assuming units are mils"));
+                    gerbv_stats_printf(error_list, GERBV_MESSAGE_WARNING, -1, _("Assuming units are mils"));
                     size /= 1000.0;
                 }
 
                 if (size <= 0. || size >= 10000.) {
                     gerbv_stats_printf(
-                        stats->error_list, GERBV_MESSAGE_ERROR, -1,
+                        error_list, GERBV_MESSAGE_ERROR, -1,
                         _("Unreasonable drill size %g found for drill %d "
                           "at line %ld in file \"%s\""),
                         size, tool_num, file_line, fd->filename
@@ -1292,7 +1288,7 @@ drill_parse_T_code(gerb_file_t* fd, drill_state_t* state, gerbv_image_t* image, 
                             || apert->nuf_parameters != 1 || apert->unit != GERBV_UNIT_INCH) {
 
                             gerbv_stats_printf(
-                                stats->error_list, GERBV_MESSAGE_ERROR, -1,
+                                error_list, GERBV_MESSAGE_ERROR, -1,
                                 _("Found redefinition of drill %d "
                                   "at line %ld in file \"%s\""),
                                 tool_num, file_line, fd->filename
@@ -1316,12 +1312,12 @@ drill_parse_T_code(gerb_file_t* fd, drill_state_t* state, gerbv_image_t* image, 
 
                 /* Add the tool whose definition we just found into the list
                  * of tools for this layer used to generate statistics. */
-                stats  = image->drill_stats;
-                string = g_strdup_printf("%s", (state->unit == GERBV_UNIT_MM ? _("mm") : _("inch")));
+                gchar* tool_description = g_strdup_printf("%s", (state->unit == GERBV_UNIT_MM ? _("mm") : _("inch")));
                 drill_stats_add_to_drill_list(
-                    stats->drill_list, tool_num, state->unit == GERBV_UNIT_MM ? size * 25.4 : size, string
+                    image->drill_stats->drill_list, tool_num, state->unit == GERBV_UNIT_MM ? size * 25.4 : size,
+                    tool_description
                 );
-                g_free(string);
+                g_free(tool_description);
                 break;
 
             default:
@@ -1332,26 +1328,24 @@ drill_parse_T_code(gerb_file_t* fd, drill_state_t* state, gerbv_image_t* image, 
                 break;
         } /* switch((char)temp) */
 
-        temp = gerb_fgetc(fd);
-        if (EOF == temp) {
+        next_char = gerb_fgetc(fd);
+        if (EOF == next_char) {
             gerbv_stats_printf(
-                stats->error_list, GERBV_MESSAGE_ERROR, -1,
+                error_list, GERBV_MESSAGE_ERROR, -1,
                 _("Unexpected EOF encountered in header of "
                   "drill file \"%s\""),
                 fd->filename
             );
-
-            /* Restore new line character for processing */
-            if ('\n' == temp || '\r' == temp) {
-                gerb_ungetc(fd);
-            }
+            done = TRUE;
+        } else if (next_char == '\n' || next_char == '\r') {
+            gerb_ungetc(fd);
+            done = TRUE;
         }
     } /* while(!done) */ /* Done looking at tool definitions */
 
     /* Catch the tools that aren't defined.
        This isn't strictly a good thing, but at least something is shown */
     if (apert == NULL) {
-        double dia;
 
         apert = image->aperture[tool_num] = g_new0(gerbv_aperture_t, 1);
         if (apert == NULL) {
@@ -1359,7 +1353,7 @@ drill_parse_T_code(gerb_file_t* fd, drill_state_t* state, gerbv_image_t* image, 
         }
 
         /* See if we have the tool table */
-        dia = gerbv_get_tool_diameter(tool_num);
+        double dia = gerbv_get_tool_diameter(tool_num);
         if (dia <= 0) {
             /*
              * There is no tool. So go out and make some.
@@ -1370,18 +1364,15 @@ drill_parse_T_code(gerb_file_t* fd, drill_state_t* state, gerbv_image_t* image, 
              * Oooh, this is sooo ugly. But some CAD systems seem to always
              * use T00 at the end of the file while others that don't have
              * tool definitions inside the file never seem to use T00 at all.
+             * tool_num already verified non-zero and in range [1..9999], above.
              */
-            if (tool_num != 0) {
-                gerbv_stats_printf(
-                    stats->error_list, GERBV_MESSAGE_ERROR, -1,
-                    _("Tool %02d used without being defined "
-                      "at line %ld in file \"%s\""),
-                    tool_num, file_line, fd->filename
-                );
-                gerbv_stats_printf(
-                    stats->error_list, GERBV_MESSAGE_WARNING, -1, _("Setting a default size of %g\""), dia
-                );
-            }
+            gerbv_stats_printf(
+                error_list, GERBV_MESSAGE_ERROR, -1,
+                _("Tool %02d used without being defined "
+                  "at line %ld in file \"%s\""),
+                tool_num, file_line, fd->filename
+            );
+            gerbv_stats_printf(error_list, GERBV_MESSAGE_WARNING, -1, _("Setting a default size of %g\""), dia);
         }
 
         apert->type           = GERBV_APTYPE_CIRCLE;
@@ -1389,16 +1380,16 @@ drill_parse_T_code(gerb_file_t* fd, drill_state_t* state, gerbv_image_t* image, 
         apert->parameter[0]   = dia;
 
         /* Add the tool whose definition we just found into the list
-         * of tools for this layer used to generate statistics. */
-        if (tool_num != 0) { /* Only add non-zero tool nums.
-                              * Zero = unload command. */
-            stats  = image->drill_stats;
-            string = g_strdup_printf("%s", (state->unit == GERBV_UNIT_MM ? _("mm") : _("inch")));
-            drill_stats_add_to_drill_list(
-                stats->drill_list, tool_num, state->unit == GERBV_UNIT_MM ? dia * 25.4 : dia, string
-            );
-            g_free(string);
-        }
+         * of tools for this layer used to generate statistics.
+         * tool_num already verified non-zero and in range [1..9999], above.
+         */
+
+        gerbv_drill_list_t* drill_list = image->drill_stats->drill_list;
+        gchar* tool_unit_string        = g_strdup_printf("%s", (state->unit == GERBV_UNIT_MM ? _("mm") : _("inch")));
+        drill_stats_add_to_drill_list(
+            drill_list, tool_num, state->unit == GERBV_UNIT_MM ? dia * 25.4 : dia, tool_unit_string
+        );
+        g_free(tool_unit_string);
     } /* if(image->aperture[tool_num] == NULL) */
 
     dprintf("<----  ...leaving %s()\n", __FUNCTION__);
