7771632158a9fe88c2236a08174ea8f7b95d4214
Henry Gabryjelski <henrygab@users.noreply.github.com>
2023-10-17 20:32:39 -0700

    formatting / alignment only

--- src/drill.c
+++ src/drill.c
@@ -2244,85 +2244,88 @@ file_check_str(gerb_file_t* fd, const char* str) {
 /** Return drill G-code name by code number. */
 const char*
 drill_g_code_name(drill_g_code_t g_code) {
+    // clang-format off
     switch (g_code) {
-        case DRILL_G_ROUT: return N_("rout mode");
-        case DRILL_G_LINEARMOVE: return N_("linear mode");
-        case DRILL_G_CWMOVE: return N_("circular CW mode");
-        case DRILL_G_CCWMOVE: return N_("circular CCW mode");
-        case DRILL_G_VARIABLEDWELL: return N_("variable dwell");
-        case DRILL_G_DRILL: return N_("drill mode");
-        case DRILL_G_OVERRIDETOOLSPEED: return N_("override tool feed or speed");
-        case DRILL_G_ROUTCIRCLE: return N_("routed CW circle");
-        case DRILL_G_ROUTCIRCLECCW: return N_("routed CCW circle");
-        case DRILL_G_VISTOOL: return N_("select vision tool");
-        case DRILL_G_VISSINGLEPOINTOFFSET: return N_("single point vision offset");
-        case DRILL_G_VISMULTIPOINTTRANS: return N_("multipoint vision translation");
-        case DRILL_G_VISCANCEL: return N_("cancel vision translation or offset");
-        case DRILL_G_VISCORRHOLEDRILL: return N_("vision corrected single hole drilling");
-        case DRILL_G_VISAUTOCALIBRATION: return N_("vision system autocalibration");
-        case DRILL_G_CUTTERCOMPOFF: return N_("cutter compensation off");
-        case DRILL_G_CUTTERCOMPLEFT: return N_("cutter compensation left");
-        case DRILL_G_CUTTERCOMPRIGHT: return N_("cutter compensation right");
+        case DRILL_G_ROUT:                    return N_("rout mode");
+        case DRILL_G_LINEARMOVE:              return N_("linear mode");
+        case DRILL_G_CWMOVE:                  return N_("circular CW mode");
+        case DRILL_G_CCWMOVE:                 return N_("circular CCW mode");
+        case DRILL_G_VARIABLEDWELL:           return N_("variable dwell");
+        case DRILL_G_DRILL:                   return N_("drill mode");
+        case DRILL_G_OVERRIDETOOLSPEED:       return N_("override tool feed or speed");
+        case DRILL_G_ROUTCIRCLE:              return N_("routed CW circle");
+        case DRILL_G_ROUTCIRCLECCW:           return N_("routed CCW circle");
+        case DRILL_G_VISTOOL:                 return N_("select vision tool");
+        case DRILL_G_VISSINGLEPOINTOFFSET:    return N_("single point vision offset");
+        case DRILL_G_VISMULTIPOINTTRANS:      return N_("multipoint vision translation");
+        case DRILL_G_VISCANCEL:               return N_("cancel vision translation or offset");
+        case DRILL_G_VISCORRHOLEDRILL:        return N_("vision corrected single hole drilling");
+        case DRILL_G_VISAUTOCALIBRATION:      return N_("vision system autocalibration");
+        case DRILL_G_CUTTERCOMPOFF:           return N_("cutter compensation off");
+        case DRILL_G_CUTTERCOMPLEFT:          return N_("cutter compensation left");
+        case DRILL_G_CUTTERCOMPRIGHT:         return N_("cutter compensation right");
         case DRILL_G_VISSINGLEPOINTOFFSETREL: return N_("single point vision relative offset");
-        case DRILL_G_VISMULTIPOINTTRANSREL: return N_("multipoint vision relative translation");
-        case DRILL_G_VISCANCELREL: return N_("cancel vision relative translation or offset");
-        case DRILL_G_VISCORRHOLEDRILLREL: return N_("vision corrected single hole relative drilling");
-        case DRILL_G_PACKDIP2: return N_("dual in line package");
-        case DRILL_G_PACKDIP: return N_("dual in line package");
-        case DRILL_G_PACK8PINL: return N_("eight pin L package");
-        case DRILL_G_CIRLE: return N_("canned circle");
-        case DRILL_G_SLOT: return N_("canned slot");
-        case DRILL_G_ROUTSLOT: return N_("routed step slot");
-        case DRILL_G_ABSOLUTE: return N_("absolute input mode");
-        case DRILL_G_INCREMENTAL: return N_("incremental input mode");
-        case DRILL_G_ZEROSET: return N_("zero set");
-
-        case DRILL_G_UNKNOWN:
-        default: return N_("unknown G-code");
+        case DRILL_G_VISMULTIPOINTTRANSREL:   return N_("multipoint vision relative translation");
+        case DRILL_G_VISCANCELREL:            return N_("cancel vision relative translation or offset");
+        case DRILL_G_VISCORRHOLEDRILLREL:     return N_("vision corrected single hole relative drilling");
+        case DRILL_G_PACKDIP2:                return N_("dual in line package");
+        case DRILL_G_PACKDIP:                 return N_("dual in line package");
+        case DRILL_G_PACK8PINL:               return N_("eight pin L package");
+        case DRILL_G_CIRLE:                   return N_("canned circle");
+        case DRILL_G_SLOT:                    return N_("canned slot");
+        case DRILL_G_ROUTSLOT:                return N_("routed step slot");
+        case DRILL_G_ABSOLUTE:                return N_("absolute input mode");
+        case DRILL_G_INCREMENTAL:             return N_("incremental input mode");
+        case DRILL_G_ZEROSET:                 return N_("zero set");
+        case DRILL_G_UNKNOWN:                 return N_("unknown G-code");
+        default:                              return N_("unknown G-code");
     }
+    // clang-format on
 } /* drill_g_code_name() */
 
 /* -------------------------------------------------------------- */
 /** Return drill M-code name by code number. */
 const char*
 drill_m_code_name(drill_m_code_t m_code) {
+    // clang-format off
     switch (m_code) {
-        case DRILL_M_END: return N_("end of program");
-        case DRILL_M_PATTERNEND: return N_("pattern end");
-        case DRILL_M_REPEATPATTERNOFFSET: return N_("repeat pattern offset");
-        case DRILL_M_STOPOPTIONAL: return N_("stop optional");
-        case DRILL_M_SANDREND: return N_("step and repeat end");
-        case DRILL_M_STOPINSPECTION: return N_("stop for inspection");
-        case DRILL_M_ZAXISROUTEPOSITIONDEPTHCTRL: return N_("Z-axis rout position with depth control");
-        case DRILL_M_ZAXISROUTEPOSITION: return N_("Z-axis rout position");
-        case DRILL_M_RETRACTCLAMPING: return N_("retract with clamping");
-        case DRILL_M_RETRACTNOCLAMPING: return N_("retract without clamping");
-        case DRILL_M_TOOLTIPCHECK: return N_("tool tip check");
-        case DRILL_M_PATTERN: return N_("pattern start");
-        case DRILL_M_ENDREWIND: return N_("end of program with rewind");
-        case DRILL_M_MESSAGELONG: return N_("long operator message");
-        case DRILL_M_MESSAGE: return N_("operator message");
-        case DRILL_M_HEADER: return N_("header start");
-        case DRILL_M_VISANDRPATTERN: return N_("vision step and repeat pattern start");
-        case DRILL_M_VISANDRPATTERNREWIND: return N_("vision step and repeat rewind");
+        case DRILL_M_END:                             return N_("end of program");
+        case DRILL_M_PATTERNEND:                      return N_("pattern end");
+        case DRILL_M_REPEATPATTERNOFFSET:             return N_("repeat pattern offset");
+        case DRILL_M_STOPOPTIONAL:                    return N_("stop optional");
+        case DRILL_M_SANDREND:                        return N_("step and repeat end");
+        case DRILL_M_STOPINSPECTION:                  return N_("stop for inspection");
+        case DRILL_M_ZAXISROUTEPOSITIONDEPTHCTRL:     return N_("Z-axis rout position with depth control");
+        case DRILL_M_ZAXISROUTEPOSITION:              return N_("Z-axis rout position");
+        case DRILL_M_RETRACTCLAMPING:                 return N_("retract with clamping");
+        case DRILL_M_RETRACTNOCLAMPING:               return N_("retract without clamping");
+        case DRILL_M_TOOLTIPCHECK:                    return N_("tool tip check");
+        case DRILL_M_PATTERN:                         return N_("pattern start");
+        case DRILL_M_ENDREWIND:                       return N_("end of program with rewind");
+        case DRILL_M_MESSAGELONG:                     return N_("long operator message");
+        case DRILL_M_MESSAGE:                         return N_("operator message");
+        case DRILL_M_HEADER:                          return N_("header start");
+        case DRILL_M_VISANDRPATTERN:                  return N_("vision step and repeat pattern start");
+        case DRILL_M_VISANDRPATTERNREWIND:            return N_("vision step and repeat rewind");
         case DRILL_M_VISANDRPATTERNOFFSETCOUNTERCTRL: return N_("vision step and repeat offset counter control");
-        case DRILL_M_REFSCALING: return N_("reference scaling on");
-        case DRILL_M_REFSCALINGEND: return N_("reference scaling off");
-        case DRILL_M_PECKDRILLING: return N_("peck drilling on");
-        case DRILL_M_PECKDRILLINGEND: return N_("peck drilling off");
-        case DRILL_M_SWAPAXIS: return N_("swap axes");
-        case DRILL_M_METRIC: return N_("metric measuring mode");
-        case DRILL_M_IMPERIAL: return N_("inch measuring mode");
-        case DRILL_M_MIRRORX: return N_("mirror image X-axis");
-        case DRILL_M_MIRRORY: return N_("mirror image Y-axis");
-        case DRILL_M_HEADEREND: return N_("header end");
-        case DRILL_M_CANNEDTEXTX: return N_("canned text along X-axis");
-        case DRILL_M_CANNEDTEXTY: return N_("canned text along Y-axis");
-        case DRILL_M_USERDEFPATTERN: return N_("user defined stored pattern");
-
-        case DRILL_M_UNKNOWN:
-        default: return N_("unknown M-code");
+        case DRILL_M_REFSCALING:                      return N_("reference scaling on");
+        case DRILL_M_REFSCALINGEND:                   return N_("reference scaling off");
+        case DRILL_M_PECKDRILLING:                    return N_("peck drilling on");
+        case DRILL_M_PECKDRILLINGEND:                 return N_("peck drilling off");
+        case DRILL_M_SWAPAXIS:                        return N_("swap axes");
+        case DRILL_M_METRIC:                          return N_("metric measuring mode");
+        case DRILL_M_IMPERIAL:                        return N_("inch measuring mode");
+        case DRILL_M_MIRRORX:                         return N_("mirror image X-axis");
+        case DRILL_M_MIRRORY:                         return N_("mirror image Y-axis");
+        case DRILL_M_HEADEREND:                       return N_("header end");
+        case DRILL_M_CANNEDTEXTX:                     return N_("canned text along X-axis");
+        case DRILL_M_CANNEDTEXTY:                     return N_("canned text along Y-axis");
+        case DRILL_M_USERDEFPATTERN:                  return N_("user defined stored pattern");
+        case DRILL_M_UNKNOWN:                         return N_("unknown M-code");
+        default:                                      return N_("unknown M-code");
     }
+    // clang-format on
+
 } /* drill_m_code_name() */
 
 /* Well, this is a bit awkward.
