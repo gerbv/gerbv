751a6bf3551df3df8fa20d03b9de70d659eb747f
rampageservices <20424586+rampageservices@users.noreply.github.com>
2026-03-02 11:31:27 +0800

    Include TinyScheme license in Windows release archive

--- .mc/windows_amd64/opt/package.sh
+++ .mc/windows_amd64/opt/package.sh
@@ -66,6 +66,7 @@ WEBSITE_DIRECTORY='gerbv.github.io/ci'
 TEMPORARY_DIRECTORY=`"${MKTEMP}" --directory`
 
 "${CP}" 'COPYING' "${TEMPORARY_DIRECTORY}"
+"${CP}" 'COPYING.tinyscheme' "${TEMPORARY_DIRECTORY}"
 "${CP}" 'src/init.scm' "${TEMPORARY_DIRECTORY}"
 "${CP}" 'build/src/Debug/gerbv.exe' "${TEMPORARY_DIRECTORY}"
 "${FIND}" 'build/src/Debug' -name 'libgerbv*.dll' -exec "${CP}" {} "${TEMPORARY_DIRECTORY}" \;
